8d970ce3155e331b7aaf522a853b6c89a92f0a37
AjayAchugatla <ajayachugatla17@gmail.com>
2024-12-14 18:41:40 +0530

    test

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -41,6 +41,8 @@ function Signup() {
             });
 
             if (response.data.token) {
+                console.log('1');
+
                 const resp = await axios.post(import.meta.env.VITE_BASE_URL + "/user/send-verify-otp", {},
                     {
                         headers: {
@@ -48,19 +50,24 @@ function Signup() {
                         }
                     }
                 )
+                console.log(2);
                 if (resp.data.error) {
+                    console.log(3);
                     setError(resp.data.error)
                     return;
                 } else {
                     localStorage.setItem("token", response.data.token)
+                    console.log(4);
                     navigate("/email-verify");
                 }
 
             } else {
+                console.log(5);
                 setError(response.data.error)
             }
 
         } catch (error) {
+            console.log(6);
             setError("An unexpected error occured. Please try Later")
         }
     }
