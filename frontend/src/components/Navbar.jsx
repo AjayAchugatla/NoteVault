42595a1eda3dab7390dbfe04ffcc93b3597c2f60
AjayAchugatla <ajayachugatla17@gmail.com>
2024-12-19 23:15:14 +0530

    Update Navbar component to conditionally display title based on 'display' prop

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar({ getSearchNotes, clearSearch, display }) {
     return (
         <div className='bg-white flex items-center justify-between px-4 py-1 drop-shadow dark:bg-black sticky w-full top-0'>
             <Link to={'/'}>
-                <h2 className='text-xl py-2 font-medium text-black sm:inline dark:text-white'>Note-Vault</h2>
+                <h2 className={`text-xl py-2 font-medium text-black sm:inline dark:text-white ${display ? 'sm:inline hidden' : 'inline'}`}>Note-Vault</h2>
             </Link>
             {display ? <>
                 <SearchBar getSearchNotes={getSearchNotes} clearSearch={clearSearch} />
